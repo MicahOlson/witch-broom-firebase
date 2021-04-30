586dba1150ea23888f7b7463f91c5a424c1b20f1
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-04-30 08:57:39 -0700

    Update kegList reducer with delete_keg action

--- src/reducers/keg-list-reducer.js
+++ src/reducers/keg-list-reducer.js
@@ -1,17 +1,21 @@
 export default (state = {}, action) => {
   const { name, brand, price, alcoholContent, pintCount, id } = action;
   switch (action.type) {
-    case 'ADD_KEG':
-      return Object.assign({}, state, {
-        [id]: {
-          name: name,
-          brand: brand,
-          price: price,
-          alcoholContent: alcoholContent,
-          pintCount: pintCount,
-          id: id
-        }
-      });
+  case 'ADD_KEG':
+    return Object.assign({}, state, {
+      [id]: {
+        name: name,
+        brand: brand,
+        price: price,
+        alcoholContent: alcoholContent,
+        pintCount: pintCount,
+        id: id
+      }
+    });
+  case 'DELETE_KEG':
+    let newState = { ...state };
+    delete newState[id];
+    return newState;
   default:
     return state;
   }  
