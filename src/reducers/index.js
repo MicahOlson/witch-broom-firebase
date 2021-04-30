63991c1759fd34c5f54e1904057e7d398be5277c
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-04-30 10:37:58 -0700

    Add test to combine reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+import formVisibleReducer from './form-visible-reducer';
+import kegListReducer from './keg-list-reducer';
+
+const rootReducer = combineReducers({
+
+});
+
+export default rootReducer;
