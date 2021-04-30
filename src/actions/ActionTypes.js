f46c86e7538a07aa8d957826a7191a0d69b96d6b
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-04-30 15:30:47 -0700

    Create action constants for all actions

---
+++ src/actions/ActionTypes.js
@@ -0,0 +1,6 @@
+export const ADD_KEG = 'ADD_KEG';
+export const DELETE_KEG = 'DELETE_KEG';
+export const TOGGLE_FORM = 'TOGGLE_FORM';
+export const SET_EDITING = 'SET_EDITING';
+export const SET_SELECTED = 'SET_SELECTED';
+export const NULL_SELECTED = 'NULL_SELECTED';
