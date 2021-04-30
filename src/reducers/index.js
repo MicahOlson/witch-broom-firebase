380cac3d843146737b089cf1c0510846a9fd4d9a
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-04-30 10:38:26 -0700

    Combine reducers into rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,7 +3,8 @@ import formVisibleReducer from './form-visible-reducer';
 import kegListReducer from './keg-list-reducer';
 
 const rootReducer = combineReducers({
-
+  formVisibleOnPage: formVisibleReducer,
+  mainKegList: kegListReducer
 });
 
 export default rootReducer;
