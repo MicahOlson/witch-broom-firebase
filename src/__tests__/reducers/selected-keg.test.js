3de46fa0d02fec7851e2a1f6d36217cb886be369
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-04-30 12:40:21 -0700

    Add and test get_keg action

--- src/__tests__/reducers/selected-keg.test.js
+++ src/__tests__/reducers/selected-keg.test.js
@@ -1,7 +1,42 @@
 import selectedKegReducer from '../../reducers/selected-keg-reducer';
 
 describe('selectedKegReducer', () => {
+  let action;
+
+  const kegData = {
+    name: 'Nectarine Premiere',
+    brand: 'de Garde Brewing',
+    price: '7',
+    alcoholContent: '7.10',
+    pintCount: 124,
+    id: 1
+  };
+
   test('Should return default state if there is no action type passed into the reducer', () => {
-    expect(selectedKegReducer({}, { type: null })).toEqual({});
+    expect(selectedKegReducer(undefined, { type: null })).toEqual(null);
+  });
+
+  test('Should successfully return selected keg from mainKegList', () => {
+    const { name, brand, price, alcoholContent, pintCount, id } = kegData;
+    action = {
+      type: 'GET_KEG',
+      name: name,
+      brand: brand,
+      price: price,
+      alcoholContent: alcoholContent,
+      pintCount: pintCount,
+      id: id
+    };
+
+    expect(selectedKegReducer({}, action)).toEqual({
+      [id]: {
+        name: name,
+        brand: brand,
+        price: price,
+        alcoholContent: alcoholContent,
+        pintCount: pintCount,
+        id: id
+      }
+    });
   });
 });
--- src/reducers/selected-keg-reducer.js
+++ src/reducers/selected-keg-reducer.js
@@ -1,3 +1,18 @@
-export default (state={}, action) => {
-  return state;
-}
+export default (state=null, action) => {
+  const { name, brand, price, alcoholContent, pintCount, id } = action;
+  switch (action.type) {
+  case 'GET_KEG':
+    return Object.assign({}, state, {
+      [id]: {
+        name: name,
+        brand: brand,
+        price: price,
+        alcoholContent: alcoholContent,
+        pintCount: pintCount,
+        id: id
+      }
+    });
+  default:
+    return state;
+  }  
+};
