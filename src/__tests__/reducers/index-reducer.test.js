ea8bfb09e6b0a138d33e0920306574de47c3bd6e
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-04-30 10:43:22 -0700

    Test if initial states of reducers match rootReducer

--- src/__tests__/reducers/index-reducer.test.js
+++ src/__tests__/reducers/index-reducer.test.js
@@ -1,4 +1,6 @@
 import { createStore } from 'redux';
+import formVisibleReducer from '../../reducers/form-visible-reducer';
+import kegListReducer from '../../reducers/keg-list-reducer';
 import rootReducer from '../../reducers/index';
 
 let store = createStore(rootReducer);
@@ -9,5 +11,13 @@ describe("rootReducer", () => {
       mainKegList: {},
       formVisibleOnPage: false
     });
+  });
+
+  test('Check that initial state of kegListReducer matches root reducer', () => {
+    expect(store.getState().mainKegList).toEqual(kegListReducer(undefined, { type: null }));
+  });
+  
+  test('Check that initial state of formVisibleReducer matches root reducer', () => {
+    expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, { type: null }));
   });
 });
