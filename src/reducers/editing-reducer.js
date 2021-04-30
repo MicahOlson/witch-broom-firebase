e9a760e9496859853e130ea8df3da88f4ff2a184
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-04-30 11:44:06 -0700

    Update editing reducer with set_edit action

--- src/reducers/editing-reducer.js
+++ src/reducers/editing-reducer.js
@@ -1,7 +1,10 @@
 export default (state=false, action) => {
+  const { editing } = action;
   switch (action.type) {
   case 'TOGGLE_EDIT':
     return !state;
+  case 'SET_EDIT':
+    return editing;
   default:
     return state;
   }
