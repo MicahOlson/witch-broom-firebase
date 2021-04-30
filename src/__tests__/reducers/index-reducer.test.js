1cd47131df1fbd075beedfa9020faa91d1e0c1af
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-04-30 11:23:04 -0700

    Add editing to rootReducer and update prior tests

--- src/__tests__/reducers/index-reducer.test.js
+++ src/__tests__/reducers/index-reducer.test.js
@@ -1,4 +1,5 @@
 import { createStore } from 'redux';
+import editingReducer from '../../reducers/editing-reducer';
 import formVisibleReducer from '../../reducers/form-visible-reducer';
 import kegListReducer from '../../reducers/keg-list-reducer';
 import rootReducer from '../../reducers/index';
@@ -8,8 +9,9 @@ let store = createStore(rootReducer);
 describe("rootReducer", () => {
   test('Should return default state if no action type is recognized', () => {
     expect(rootReducer({}, { type: null })).toEqual({
-      mainKegList: {},
-      formVisibleOnPage: false
+      editing: false,
+      formVisibleOnPage: false,
+      mainKegList: {}
     });
   });
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
+import editingReducer from './editing-reducer';
 import formVisibleReducer from './form-visible-reducer';
 import kegListReducer from './keg-list-reducer';
 
 const rootReducer = combineReducers({
+  editing: editingReducer,
   formVisibleOnPage: formVisibleReducer,
   mainKegList: kegListReducer
 });
