4db6c308af4b41c13707ce748926839a2d2c3715
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-04-30 11:13:15 -0700

    Add red-stage editing reducer test

--- src/reducers/keg-list-reducer.js
+++ src/reducers/keg-list-reducer.js
@@ -1,4 +1,4 @@
-export default (state = {}, action) => {
+export default (state={}, action) => {
   const { name, brand, price, alcoholContent, pintCount, id } = action;
   switch (action.type) {
   case 'ADD_KEG':
