6bdffcf68aa10b50fa4f08c27dcba12ea844f09a
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-04-30 08:36:09 -0700

    Add red-stage test for add_keg action

--- src/__tests__/reducers/keg-list-reducer.test.js
+++ src/__tests__/reducers/keg-list-reducer.test.js
@@ -1,7 +1,41 @@
 import kegListReducer from '../../reducers/keg-list-reducer';
 
 describe('kegListReducer', () => {
+  let action;
+  const kegData = {
+    name: 'Nectarine Premiere',
+    brand: 'e Garde Brewing',
+    price: '7',
+    alcoholContent: '7.10',
+    pintCount: 124,
+    id: 1
+  };
+
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(kegListReducer({}, { type: null })).toEqual({});
+  });
+
+  test('Should successfully add new keg data to mainKegList', () => {
+    const { name, brand, price, alcoholContent, pintCount, id } = kegData;
+    action = {
+      type: 'ADD_KEG',
+      name: name,
+      brand: brand,
+      price: price,
+      alcoholContent: alcoholContent,
+      pintCount: pintCount,
+      id: id
+    };
+
+    expect(kegListReducer({}, action)).toEqual({
+      [id]: {
+        name: name,
+        brand: brand,
+        price: price,
+        alcoholContent: alcoholContent,
+        pintCount: pintCount,
+        id: id
+      }
+    });
   });
 });
