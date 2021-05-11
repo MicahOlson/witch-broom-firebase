8ed1d7b870742789cee7e03e81719d8453009396
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-05-11 11:28:52 -0700

    Add signin route and links

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './Header';
 import KegControl from './KegControl';
+import Signin from './Signin';
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
-      <KegControl />
-    </>
+      <Switch>
+        <Route path='/signin'>
+          <Signin />
+        </Route>
+        <Route path='/'>
+          <KegControl />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
     <>
       <h1>The Witch&apos;s Broom</h1>
-      <h2>Tap Room Kegs</h2>
+      <h2>Keg Kollector</h2>
+      <p><Link to='/'>Home</Link> | <Link to='/signin'>Sign In</Link></p>
     </>
   )
 }
