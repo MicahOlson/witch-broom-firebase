5383355338dacc6421cb6f04981b9c690b8989c6
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-05-11 14:43:44 -0700

    Add styled components to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const WitchBroomHeader = styled.h1`
+  font-size: 36px;
+  text-align: center;
+  color: green;
+  text-shadow: 1px 1px gray;
+  margin-bottom: 0px;
+`
+
+const WitchBroomSubheader = styled.h2`
+  text-align: center;
+  text-shadow: 1px 1px gray;
+  margin-top: 0px;
+  margin-bottom: 100px;
+`
 
 function Header() {
   return (
     <>
-      <h1>The Witch&apos;s Broom</h1>
-      <h2>Keg Kollector</h2>
+      <WitchBroomHeader>
+        The Witch&apos;s Broom
+      </WitchBroomHeader> 
+      <WitchBroomSubheader>
+        Keg Kollector
+      </WitchBroomSubheader>
       <p><Link to='/'>Home</Link> | <Link to='/signin'>Sign In</Link></p>
     </>
   )
